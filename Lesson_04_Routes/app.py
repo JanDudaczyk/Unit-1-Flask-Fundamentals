70987393a8e0a66a9aa91786b69bc2735e38c734
Jan Dudaczyk <jandud27@bergen.org>
2025-09-18 09:39:15 -0400

    removed env files

---
+++ Lesson_04_Routes/app.py
@@ -0,0 +1,55 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return f'''
+           <h1>Dynamic Routes Demo</h1>
+<h2>Try: These URLS</h2>
+<ul>
+    <li><a href="/user/john">User Profile: john</a></li>
+    <li><a href="/user/alice">User Profile: alice</a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+</ul> 
+'''
+
+@app.route('/user/<username>')
+def user_profile(username):
+    return f'''
+     <h1>User Profile</h1>
+ <p>Username: <strong>{username}</strong></p>
+ <p>Profile Type: {type(username).__name__}</p>
+ <p>Welcome to {username}'s profile page!</p>
+ <nav>
+    <a href="/"> Back to Homepage</a>
+    <a href="/user/alice">Alice</a>
+    <a href="/user/bob">Bob</a>
+ </nav>    
+'''
+@app.route("/calc/<int:num1>/<operation>/<int:num2>")
+def calculator(num1, operation, num2):
+    operations = {
+        '+' : num1 + num2,
+        '-' : num1 - num2,
+        '*' : num1 * num2,
+        '/' : num1 / num2 if num2 != 0 else 'Error Division by Zero!'
+    }
+    if operation in operations:
+        result = operations[operation]
+        return f"{num1} {operation} {num2} = {result}"
+    else:
+        return f"Unknown Operation!{operation}"
+@app.route("/temp_convert/<int:temp1>/<conversion_factor>/<temp_final>")
+def temp_converter(temp1, conversion_factor, temp_final):
+    conversion_factor = {temp1} * 5/9 - 32
+    temp_final = conversion_factor
+    return f"{temp1} when converted is {temp_final}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
